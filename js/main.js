af8deb366df1a6ef91bba904837ea8d2b14163a5
claudiasanes <claudiasanes@gmail.com>
2020-07-13 13:55:06 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 'use strict';
 
-const btn = document.querySelector('.js-button');
 const input = document.querySelector('.js-input');
 const clue = document.querySelector('.js-clue');
 const counter = document.querySelector('.js-counter');
 
-let counterNumber = parseInt(counter.innerHTML);
+function addButtonListener() {
+  const btn = document.querySelector('.js-button');
+  btn.addEventListener('click', guessTheNumber);
+}
+
+addButtonListener();
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const randomNumber = getRandomNumber(100);
-console.log(randomNumber);
 
 function guessTheNumber(ev) {
+  const randomNumber = getRandomNumber(100);
   ev.preventDefault();
   let inputValue = input.value;
-
   if (inputValue > 100 || inputValue < 1) {
     updateCounter();
-    clue.innerHTML = 'El número debe estar entre 1 y 100';
+    clue.innerHTML = 'El número debe estar entre 1 y 100 🙂';
   } else if (inputValue > randomNumber) {
     updateCounter();
-    clue.innerHTML = 'Demasiado alto';
+    clue.innerHTML = 'Demasiado alto 😉';
   } else if (inputValue < randomNumber) {
     updateCounter();
-    clue.innerHTML = 'Demasiado bajo';
+    clue.innerHTML = 'Demasiado bajo 😉';
   } else {
     updateCounter();
-    clue.innerHTML = 'Has ganado campeona!!!';
+    clue.innerHTML = '🔮Lo has adivinado 🔮';
   }
 }
 
 function updateCounter() {
+  let counterNumber = parseInt(counter.innerHTML);
   counterNumber = counterNumber + 1;
-  console.log(counterNumber);
   counter.innerHTML = counterNumber;
 }
-
-btn.addEventListener('click', guessTheNumber);
